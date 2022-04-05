5c5bd1265ec253cbcf8d8dc958170e6f730536c9
Bobby Schwartz <robertschwartz603@gmail.com>
2022-04-04 22:00:36 -0400

    repairs to homeRoutes, helpers, and minor handlebars updates

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,8 @@
 module.exports = {
     format_date: (date) => {
+      if (!date) {
+        return date;
+      }
       // Format date as MM/DD/YYYY
       return date.toLocaleDateString();
     },
