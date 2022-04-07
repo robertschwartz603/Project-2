6c8369a7b5e47dddf29bf496e830cfa839a3ec98
Prathibha <pratibha.indel@gmail.com>
2022-04-06 21:25:46 -0400

    updated pics get_emoji

--- utils/helpers.js
+++ utils/helpers.js
@@ -12,15 +12,28 @@ module.exports = {
     },
     get_emoji: () => {
       const randomNum = Math.random();
-  
-      // Return a random emoji
-      if (randomNum > 0.7) {
-        return `<span for="img" aria-label="lightbulb">💡</span>`;
+       // Return a random emoji
+       if (randomNum > 0.7) {
+        return `<span><figure class="img ">
+        <img class="manimg"src="assets/pic4.jpg" alt="restaurant pics"/>
+        </figure></span>`;
       } else if (randomNum > 0.4) {
-        return `<span for="img" aria-label="laptop">💻</span>`;
-      } else {
-        return `<span for="img" aria-label="gear">⚙️</span>`;
+        return `<span ><span><figure class="img ">
+        <img class="manimg"src="assets/picA.jpg" alt="restaurant pics"/>
+        </figure></span></span>`;
+      }else if (randomNum > 0.2) {
+        return `<span ><span><figure class="img ">
+        <img class="manimg"src="assets/picB.jpg" alt="restaurant pics"/>
+        </figure></span></span>`;
       }
+      
+      else {
+        return  `<span ><span><figure class="img ">
+        <img class="manimg"src="assets/picC.jpg" alt="restaurant pics"/>
+        </figure></span></span>`;
+      }
+  
+     
     },
   };
   
